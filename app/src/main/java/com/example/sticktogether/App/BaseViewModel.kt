b9ea9c1759d8198a759d78eed0e72a6cf38b6302
TenHenry <marcinuznanski2@gmail.com>
2026-04-12 20:52:49 +0200

    Created UiEvent.kt + BaseViewModel.kt

---
+++ app/src/main/java/com/example/sticktogether/App/BaseViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.sticktogether.App
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val _uiEvent = MutableSharedFlow<UiEvent>()
+
+    val uiEvent: SharedFlow<UiEvent> = _uiEvent.asSharedFlow()
+
+    protected fun sendEvent(event: UiEvent) {
+        viewModelScope.launch {
+            _uiEvent.emit(event)
+        }
+    }
+}
